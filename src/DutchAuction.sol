54150c401050fb94c7b7b77a2cea94d540900950
GaurangBrdv <gaurang1415@gmail.com>
2025-06-10 15:32:17 +0530

    Updated DutchAuction

--- src/DutchAuction.sol
+++ src/DutchAuction.sol
@@ -5,82 +5,86 @@ pragma solidity 0.8.24;
  This dutch auction allows user to sell their goods with desired price, the price will gose down by the time pass by defined discountRate set by seller.
 */
 contract DutchAuction {
-    error AuctionIsNotActive();
+    error DutchAuction__AuctionIsNotActive();
+    error DutchAuction__SendEnoughMoney();
+    error DutchAuction__TransactionFailed();
+    error DutchAuction__CallerIsNotSeller();
+    error DutchAuction__GoodIsSold();
 
-    address owner;
-    uint256 count = 0;
-    uint256 public constant duration = 7 days;
+    address public seller;
+    string public description;
+    uint256 public startingPrice;
+    uint256 public discountRate;
+    uint256 public timestamp;
+    uint256 public duration;
+    uint256 public sellerBalance;
+    bool public isActive = false;
+    bool public isSold = false;
 
-    struct Auction {
-        address payable seller;
-        address buyer;
-        string description;
-        uint256 price;
-        uint256 discountRate;
-        uint256 timestamp;
-        uint256 duration;
+    constructor(address _seller, string memory _description, uint256 _price, uint256 _discountRate, uint256 _duration) {
+        seller = _seller;
+        description = _description;
+        startingPrice = _price;
+        discountRate = _discountRate;
+        timestamp = block.timestamp;
+        duration = timestamp + (_duration * 1 days);
+        isActive = true;
     }
 
-    mapping(uint256 => Auction) public auctions;
-    mapping(address seller => uint256 balance) private userBalance;
-
-    constructor() {
-        owner = msg.sender;
-    }
-
-    function addBid(string memory _description, uint256 _price, uint256 _rate) external returns (Auction memory) {
-        require(_rate <= 5, "discount rate is too high");
-        require(_price >= _rate * duration, "starting price < min");
-        Auction memory auction = Auction({
-            seller: payable(msg.sender),
-            buyer: address(0),
-            description: _description,
-            price: _price * 1 ether,
-            discountRate: _rate,
-            timestamp: block.timestamp,
-            duration: block.timestamp + duration
-        });
-        auctions[count] = auction;
-        count = count + 1;
-        return auction;
-    }
-
-    function getPrice(uint256 id) public view returns (uint256) {
-        Auction memory auction = auctions[id];
-        if (block.timestamp > auction.duration) {
-            revert AuctionIsNotActive();
+    function getPrice() public view returns (uint256) {
+        if (isSold) {
+            revert DutchAuction__GoodIsSold();
         }
-        uint256 timepassed = block.timestamp - auction.timestamp;
-        uint256 goodPrice = auction.price;
-
-        uint256 discount = (auction.price * auction.discountRate * timepassed) / 10000;
-        return goodPrice - discount;
+        if (!isActive) {
+            revert DutchAuction__AuctionIsNotActive();
+        }
+        if (block.timestamp > duration) {
+            revert DutchAuction__AuctionIsNotActive();
+        }
+        uint256 timeElapsed = block.timestamp - timestamp;
+        uint256 discount = discountRate * timeElapsed;
+        return startingPrice - discount;
     }
 
-    function buyGood(uint256 id) external payable {
-        Auction storage auction = auctions[id];
-        require(auction.buyer == address(0), "Item already sold");
-        require(block.timestamp <= auction.duration, "Auction is not active");
-        require(msg.sender != auction.seller, "Seller can not but their own good");
-        uint256 currentPrice = getPrice(id);
-        require(msg.value >= currentPrice, "You don't have enough money");
-        userBalance[auction.seller] += currentPrice;
-        auction.buyer = msg.sender;
-        auction.price = 0;
-        delete auctions[id];
-        (bool ok,) = payable(msg.sender).call{value: msg.value - currentPrice}("");
-        require(ok, "transfer failed");
+    function buyGood() external payable {
+        if (!isActive) {
+            revert DutchAuction__AuctionIsNotActive();
+        }
+        if (block.timestamp > duration) {
+            revert DutchAuction__AuctionIsNotActive();
+        }
+
+        if (isSold) {
+            revert DutchAuction__GoodIsSold();
+        }
+
+        uint256 goodPrice = getPrice();
+        if (msg.value < goodPrice) {
+            revert DutchAuction__SendEnoughMoney();
+        }
+        isSold = true;
+        uint256 refund = msg.value - goodPrice;
+        if (refund > 0) {
+            (bool ok,) = payable(msg.sender).call{value: refund}("");
+            if (!ok) {
+                revert DutchAuction__TransactionFailed();
+            }
+        }
+
+        sellerBalance = goodPrice;
     }
 
     function withdraw() external {
-        uint256 amount = userBalance[msg.sender];
-        require(amount > 0, "No balance to withdraw");
-        userBalance[msg.sender] = 0;
-        (bool ok,) = payable(msg.sender).call{value: amount}("");
-        require(ok, "transaction failed");
-    }
+        if (msg.sender != seller) {
+            revert DutchAuction__CallerIsNotSeller();
+        }
+        uint256 balance = sellerBalance;
+        sellerBalance = 0;
+        isActive = false;
+        (bool ok,) = payable(seller).call{value: balance}("");
 
-    function getUserBalance(address user) external view returns (uint256) {
-        return userBalance[user];
+        if (!ok) {
+            revert DutchAuction__TransactionFailed();
+        }
     }
 }
---
+++ src/DutchFactory.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {DutchAuction} from "./DutchAuction.sol";
+
+contract AuctionFactory {
+    error AuctionFactory__NonOwnerCanNotWithdraw();
+    error AuctionFactory__SendProperFees();
+    error AuctionFactory__SetProperPrice();
+    error AuctionFactory__TransactionFailed();
+    error AuctionFactory__NotOwner();
+
+    address private owner;
+    uint256 public constant FEES = 0.15 ether;
+    uint256 public count = 0;
+    mapping(uint256 id => DutchAuction auction) public auctions;
+    mapping(address seller => uint256 id) public sellerToAuction;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function startAuction(string memory _description, uint256 _price, uint256 _discountRate, uint256 _duration)
+        external
+        payable
+    {
+        if (msg.value != FEES) {
+            revert AuctionFactory__SendProperFees();
+        }
+        if (_price < _discountRate * _duration) {
+            revert AuctionFactory__SetProperPrice();
+        }
+        DutchAuction auction = new DutchAuction(msg.sender, _description, _price, _discountRate, _duration);
+
+        auctions[count] = auction;
+        sellerToAuction[msg.sender] = count;
+        count += 1;
+    }
+
+    function withdraw() external {
+        if (msg.sender != owner) {
+            revert AuctionFactory__NonOwnerCanNotWithdraw();
+        }
+        (bool ok,) = payable(owner).call{value: address(this).balance}("");
+
+        if (!ok) {
+            revert AuctionFactory__TransactionFailed();
+        }
+    }
+
+    function cleanUp() external {
+        if (msg.sender != owner) {
+            revert AuctionFactory__NotOwner();
+        }
+        uint256 length = count;
+
+        for (uint256 i = 0; i < length; i++) {
+            DutchAuction auction = auctions[i];
+            address seller = auction.seller();
+            if (!auction.isActive()) {
+                delete auctions[i];
+                delete sellerToAuction[seller];
+            }
+        }
+    }
+}
